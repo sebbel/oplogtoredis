d64e16cf1c265997cdab894b0bd4e91410a52de4
sebbel <sebbel@users.noreply.github.com>
2021-11-17 12:27:21 +0100

    update default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
-{ sources ? import ./nix/sources.nix {}, pkgs ? import sources.nixpkgs {}, oplog ? sources.oplogtoredis }:
-with pkgs;
-
-buildGoModule {
-  name = "oplogtoredis";
-  src = oplog;
-  checkPhase = false;
-  vendorSha256 = "19qnprrcmbqv94ak2m85g0mdr0cid00hym2lxpbyb16k7wm4s7ia";
+{ sources ? import ./nix/sources.nix { }, pkgs ? import sources.nixpkgs { }
+, oplog ? sources.oplogtoredis }:
+with pkgs; {
+  oplogtoredis = buildGoModule {
+    name = "oplogtoredis";
+    src = oplog;
+    checkPhase = false;
+    vendorSha256 = "19qnprrcmbqv94ak2m85g0mdr0cid00hym2lxpbyb16k7wm4s7ia";
+  };
 }
